091c588356443aa1f034ae2e983fa611b5da3c82
Srinivas <mailtokyatham@gmail.com>
2024-07-17 19:50:31 +1000

    updated with sample tests

--- src/test/java/org/scripts/StartUpAppium.java
+++ src/test/java/org/scripts/StartUpAppium.java
@@ -1,13 +1,35 @@
 package org.scripts;
 
 
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.webdriverutil.AppiumDriver;
 
 public class StartUpAppium {
-    public static void main(String[] args) {
-        try{
+    static AndroidDriver driver=null;
+    @BeforeClass
+    public static void InitiateDriver(){
+            try{
 
-            AppiumDriver.getAndriodDriverInstant(System.getProperty("user.dir")+"/src/test/resources/ApiDemos-debug.apk");
+                driver = AppiumDriver.getAndriodDriverInstant(System.getProperty("user.dir")+"/src/test/resources/ApiDemos-debug.apk");
+
+            }
+            catch (Exception e){
+                e.fillInStackTrace();
+            }
+    }
+    @Test
+    public void testPreference(){
+        try{
+            driver.findElement(By.xpath("//android.widget.TextView[@text='Preference']")).click();
+            driver.findElement(By.xpath("//android.widget.TextView[@text='3. Preference dependencies']")).click();
+            driver.findElement(By.xpath("//android.widget.CheckBox[@resource-id='android:id/checkbox']")).click();
+            driver.findElement(By.xpath("//android.widget.TextView[@text='WiFi settings']")).click();
+            driver.findElement(By.xpath("//android.widget.EditText[@resource-id='android:id/edit']")).sendKeys("Home");
+            driver.findElement(By.xpath("//android.widget.Button[@text='OK']")).click();
+
 
         }
         catch (Exception e){
